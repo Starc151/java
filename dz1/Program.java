af6623137b548ce826390b723b80ba617eb49f48
Aleksey Pashkov <dr.Starc@ya.ru>
2023-01-25 10:00:00 +0300

    fix cyrillic

--- dz1/Program.java
+++ dz1/Program.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class Program {
     public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
-    System.out.print("¬ведите число num: ");
+    System.out.print("Enter a number: ");
     float num = in.nextFloat();
     float numT = (num/2)*(num+1);
     int Treagle = (int) numT;
-    System.out.printf("“реугольное число num: %d \n", Treagle);
+    System.out.printf("Triangular number: %d \n", Treagle);
         in.close();
     }
 }

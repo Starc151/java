02f65181c9fde513b12d2d284b527c22054a4560
Aleksey Pashkov <dr.Starc@ya.ru>
2023-01-27 12:18:51 +0300

    add check num

--- dz1/Program.java
+++ dz1/Program.java
@@ -7,15 +7,21 @@ public class Program {
 
     public static int inputNum(){
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter a number: ");
+        System.out.print("Enter a number greater than 0: ");
+        if (!in.hasNextInt()) {
+            System.out.println("You didn't enter a number");
+            System.exit(0);
+        }
         int num = in.nextInt();
+        if (num <=0) {
+            System.out.println("You entered a number less than 0");
+            System.exit(0);
+        }
         return num;
     }
 
-    public static int calcTriangularNumber(float num){
-        int result = Math.round((num/2)*(num+1));
-        return result; 
-
+    public static int calcTriangularNumber(int num){
+        return (num*(num+1))/2;
     }
 
     public static void getTriangularNumber(){
